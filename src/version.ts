255f987b925be9d4853322b3c8114dbdeff90c91
Martin Lecat <martinlecat51@gmail.com>
2024-05-06 21:07:40 +0200

    feat: Changed behavior to be SemVer compliant

--- src/version.ts
+++ src/version.ts
@@ -1,57 +1,146 @@
 import type { VersionOptions } from "oss-version-js";
 
+/**
+ * Regex101 page: {@link https://regex101.com/r/vkijKf/1/}
+ */
+const SemanticVersionRegexp =
+    /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+
 export class Version {
-    private input: string;
     private options: VersionOptions;
     private major: number;
     private minor: number;
     private patch: number;
-    private type: string;
+    private preRelease: string | undefined;
+    private buildMetadata: string | undefined;
 
     static DEFAULT_OPTIONS: VersionOptions = {
-        minMajor: 0,
-        maxMajor: 10,
-        minMinor: 0,
-        maxMinor: 10,
-        minPatch: 0,
-        maxPatch: 10,
-        type: "short",
-        defaultType: "release",
+        showBuildmeta: false,
+        showPreRelease: true,
     };
 
     constructor(input: string, options: Partial<VersionOptions> = {}) {
-        this.input = input;
         this.options = {
             ...Version.DEFAULT_OPTIONS,
             ...options,
         };
 
-        let tmp = Version.parse(input, options);
+        let tmp = Version.parse(input);
         this.major = tmp.major;
         this.minor = tmp.minor;
         this.patch = tmp.patch;
-        this.type = tmp.type;
+        this.preRelease = tmp.preRelease;
+        this.buildMetadata = tmp.buildMetadata;
     }
-    toString(showType = true) {
-        let ver = `${this.major}.${this.minor}.${this.patch}`;
-        if (showType) {
-            ver += `-${this.options.type === "short" ? this.type.at(0) : this.type}`;
+    incrementMajor(by = 1) {
+        by = Math.round(by);
+
+        if (by <= 0) {
+            throw new Error(`Can't increase by ${by}. ${by} must be superior to 0`);
         }
-        return ver;
+        this.major += by;
+        return this;
     }
-    static parse(input: string, options: Partial<VersionOptions> = {}) {
-        let opt = {
+    decrementMajor(by = 1) {
+        by = Math.round(by);
+        if (by <= 0) {
+            throw new Error(`Can't decrease by ${by}. ${by} must be superior to 0`);
+        }
+        if (this.major - by < 0) {
+            throw new Error(`Can't decrease by ${by} major version (would be negative)`);
+        }
+
+        this.major -= by;
+        return this;
+    }
+    setMajor(at: number) {
+        if (at < 0) {
+            throw new Error(`Can't set major version at ${at}. ${at} must be superior or equal to 0`);
+        }
+    }
+
+    incrementMinor(by = 1) {
+        by = Math.round(by);
+
+        if (by <= 0) {
+            throw new Error(`Can't increase by ${by}. ${by} must be superior to 0`);
+        }
+        this.minor += by;
+        return this;
+    }
+    decrementMinor(by = 1) {
+        by = Math.round(by);
+        if (by <= 0) {
+            throw new Error(`Can't decrease by ${by}. ${by} must be superior to 0`);
+        }
+        if (this.minor - by < 0) {
+            throw new Error(`Can't decrease by ${by} minor version (would be negative)`);
+        }
+
+        this.minor -= by;
+        return this;
+    }
+
+    incrementPatch(by = 1) {
+        by = Math.round(by);
+
+        if (by <= 0) {
+            throw new Error(`Can't increase by ${by}. ${by} must be superior to 0`);
+        }
+        this.patch += by;
+        return this;
+    }
+    decrementPatch(by = 1) {
+        by = Math.round(by);
+        if (by <= 0) {
+            throw new Error(`Can't decrease by ${by}. ${by} must be superior to 0`);
+        }
+        if (this.patch - by < 0) {
+            throw new Error(`Can't decrease by ${by} patch version (would be negative)`);
+        }
+
+        this.patch -= by;
+        return this;
+    }
+
+    getObject() {
+        return {
+            major: this.major,
+            minor: this.minor,
+            patch: this.patch,
+            preRelease: this.preRelease,
+            buildMetadata: this.buildMetadata,
+        };
+    }
+    toString(options: Partial<VersionOptions> = {}) {
+        const opt = {
             ...Version.DEFAULT_OPTIONS,
+            ...this.options,
             ...options,
         };
 
-        let tmp = input.split("-");
-        let nums = tmp[0]?.split(".") ?? [];
-        return {
-            major: Number(nums[0]) ?? opt.minMajor,
-            minor: Number(nums[1]) ?? opt.minMinor,
-            patch: Number(nums[2]) ?? opt.minPatch,
-            type: tmp[1] ?? opt.defaultType,
-        };
+        let ver = `${this.major}.${this.minor}.${this.patch}`;
+        if (opt.showPreRelease && this.preRelease) {
+            ver += `-${this.preRelease}`;
+        }
+        if (opt.showBuildmeta && this.buildMetadata) {
+            ver += `+${this.buildMetadata}`;
+        }
+        return ver;
+    }
+    static parse(input: string) {
+        const match = input.match(SemanticVersionRegexp);
+
+        if (match) {
+            return {
+                major: Number(match[1]),
+                minor: Number(match[2]),
+                patch: Number(match[3]),
+                preRelease: match[4],
+                buildMetadata: match[5],
+            };
+        } else {
+            throw new Error(`${input} is not Semantic Version compliant. See more here: https://semver.org/`);
+        }
     }
 }
